9cee104424940dbf68cc59cf1caaed9568dc26de
Zack <lzcers@gmail.com>
2025-05-22 00:56:08 +0800

    feat(clocker): 添加状态管理并重构计时器功能
    引入 `ClockerStatus` 枚举来管理计时器的状态，并重构 `is_running` 方法以使用状态判断。此外，将 `duration` 方法重命名为 `get_duration`，并将 `get_current_timestamp` 方法改为公开方法，以增强代码的可读性和可维护性。

--- src/clocker.rs
+++ src/clocker.rs
@@ -1,10 +1,18 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
+#[derive(PartialEq)]
+enum ClockerStatus {
+    Wating,
+    Running,
+    Stopped,
+}
+
 // 一个计时器
 // 应用可以在任何时间创建一个计时器，并将其关联到一个任务上
 pub struct Clocker {
     start_time: Option<u64>,
     end_time: Option<u64>,
+    status: ClockerStatus,
 }
 
 impl Clocker {
@@ -12,16 +20,23 @@ impl Clocker {
         Clocker {
             start_time: None,
             end_time: None,
+            status: ClockerStatus::Wating,
         }
     }
 
     pub fn start(&mut self) {
         self.start_time = Some(Self::get_current_timestamp());
+        self.status = ClockerStatus::Running;
     }
 
     pub fn stop(&mut self) {
         let end_time = Self::get_current_timestamp();
         self.end_time = Some(end_time);
+        self.status = ClockerStatus::Stopped;
+    }
+
+    pub fn get_status(&self) -> &ClockerStatus {
+        &self.status
     }
 
     pub fn get_start_time(&self) -> u64 {
@@ -32,7 +47,7 @@ impl Clocker {
         self.end_time
     }
 
-    pub fn duration(&self) -> u64 {
+    pub fn get_duration(&self) -> u64 {
         if !self.is_running() {
             self.get_end_time()
                 .expect("The clocker was stopped, but get end_time failed!")
@@ -43,10 +58,14 @@ impl Clocker {
     }
 
     pub fn is_running(&self) -> bool {
-        self.end_time.is_none()
+        if self.status == ClockerStatus::Running {
+            true
+        } else {
+            false
+        }
     }
 
-    fn get_current_timestamp() -> u64 {
+    pub fn get_current_timestamp() -> u64 {
         SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("The clocker get start time error")
